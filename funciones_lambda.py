1211b823138b874c93f4c13de61ea5675e09b31f
Julian Bernal <Julian-1705@hotmail.com>
2022-07-16 12:32:29 -0500

    Commit donde agrego Funciones lambda y funciones de orden superior

--- funciones_lambda.py
+++ funciones_lambda.py
@@ -1,6 +1,30 @@
 # Lambda son funciones anónimas ya que no tienen identificador o nombre.
-# Aqui un ejemplo para con un programa que nos dice si una palabra es palindromo.
+# Aqui un ejemplo basico con un programa que nos dice si una palabra es palindromo.
 
-palindromo = lambda string: string == string[::-1]
+# def run():
+#     palindromo = lambda string: string == string[::-1]
+#     print (palindromo('ana'))
 
-print (palindromo('ana'))
+
+# if __name__=='__main__':
+#     run()
+
+# la funcion de orden superior es la que recibe como parametro otra funcion y 
+# hace algo con ella.
+
+def saludo(func):
+    func()
+
+def hola():
+    print ('Hola a todos')
+
+def adios():
+    print ('Chao')
+
+saludo(hola)
+saludo(adios)
+
+# Existen 3 funciones de orden superior:
+# 1. filter()
+
+
