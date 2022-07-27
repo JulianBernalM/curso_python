5f3ff5ebbc08061bf8ef2181056e4325e26da5b0
Julian Bernal <Julian-1705@hotmail.com>
2022-07-27 09:41:11 -0500

    Commit con practica de try-except-raise

--- debugging.py
+++ debugging.py
@@ -19,20 +19,27 @@
 # El Debugging se encuentra en la parte izquierda de VSC.
 # Tambien podre agregar un BreackPoint donde se pausara el programa despues de ejecutarse con el Debug.
 
+# Poniendo a prueba el manejo de excepciones.
+# Programa que permite ingresar un numero y darnos como resultado los 
+# numeros que son divisibles por ese numero
+# ¿Que pasaria si el usuario ingresara una letra o un numero negativo?
 
 def divider(number):
-    list = [i for i in range(1,number+1) if number%i == 0]
-    return list
-
+    try:
+        if number < 0:
+            raise ValueError("Solo numeros positivos")
+        list = [i for i in range(1,number+1) if number%i == 0]
+        return list
+    except ValueError as V:
+        print (V)
+        return False
 
 def run():
     try:
         number = int(input('Ingrese un numero: '))
-        if number == string:
-            raise ValueError('No se pueden ingresar palabras')
         print (divider(number))
-    except ValueError as ve:
-        print(ve)
+    except ValueError:
+        print ('Solo puede ingresar numeros')        
         print ('Termino mi programa')
 
 
