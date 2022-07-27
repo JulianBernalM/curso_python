da42db1001d239590c0c2ae595878fd2ef295e6f
Julian Bernal <Julian-1705@hotmail.com>
2022-07-27 09:00:07 -0500

    Commit con ultimas modificaciones se modifica el debugging y se agrega el try except raise finally

--- debugging.py
+++ debugging.py
@@ -1,20 +1,40 @@
 # Funcion que recibe un numero como parametro y retorna una lista
 # con todos los divisores de este numero
 
+# def divider(number):
+#     list = [i for i in range(1,number+1) if number%i == 0]
+#     return list
+
+
+# def run():
+#     number = int(input('Ingrese un numero: '))
+#     print (divider(number))
+#     print ('Termino mi programa')
+
+
+# if __name__=="__main__":
+#     run()
+
+# Para hacer un Debugging debo recorrer el programa linea por linea hasta encontrar el error
+# El Debugging se encuentra en la parte izquierda de VSC.
+# Tambien podre agregar un BreackPoint donde se pausara el programa despues de ejecutarse con el Debug.
+
+
 def divider(number):
     list = [i for i in range(1,number+1) if number%i == 0]
     return list
 
 
 def run():
-    number = int(input('Ingrese un numero: '))
-    print (divider(number))
-    print ('Termino mi programa')
+    try:
+        number = int(input('Ingrese un numero: '))
+        if number == string:
+            raise ValueError('No se pueden ingresar palabras')
+        print (divider(number))
+    except ValueError as ve:
+        print(ve)
+        print ('Termino mi programa')
 
 
 if __name__=="__main__":
     run()
-
-# Para hacer un Debugging debo recorrer el programa linea por linea hasta encontrar el error
-# El Debugging se encuentra en la parte izquierda de VSC.
-# Tambien podre agregar un BreackPoint donde se pausara el programa despues de ejecutarse con el Debug.
