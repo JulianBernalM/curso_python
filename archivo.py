453d93c5a4cbf83edf58e547799dc144e87a760e
Julian Bernal <Julian-1705@hotmail.com>
2022-07-28 08:10:14 -0500

    Commit con programa que trabaja con archivos

---
+++ archivo.py
@@ -0,0 +1,34 @@
+# Lee un archivo que contiene una serie de numeros
+# Convierte cada linea en una lista para poderla trabajar
+# encoding='utf-8' Nos asegura de que no existan caracteres extraños
+ 
+def read():
+    numbers = []
+
+    with open('./archivos/numbers.txt', 'r', encoding='utf-8') as f:
+        for line in f:
+            numbers.append(int(line))#Convertimos la cadena a entero
+    print(numbers)
+
+# Crea un archivo que contenga en cada linea
+# la lista de nombres
+# Puedo cambiar el segundo parametro de open por la letra a
+# Los elementos que estan en la lista names se agregaran y no es
+# sobreescribiran    
+ 
+def write():
+    names = ['Julian', 'Adriana', 'Daniela', 'Andrea', 
+    'Rocío', 'Birman', 'Rosemberg', 'Catalina']
+    
+    with open('./archivos/names.txt', 'w', encoding='utf-8') as f:
+        for name in names:
+            f.write(name)
+            f.write('\n')
+
+
+def run():
+    write()
+
+
+if __name__=='__main__':
+    run()
